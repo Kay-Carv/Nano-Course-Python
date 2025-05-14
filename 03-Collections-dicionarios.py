db2d7191933560480d7a94d175699d445f5821fe
Kayque <kayquecarvalho020@gmail.com>
2025-05-13 23:21:57 -0300

    Add: Conteúdo de OrderedDict - Mantem a ordem nos itens de dicionário

--- 03-Collections-dicionarios.py
+++ 03-Collections-dicionarios.py
@@ -45,3 +45,64 @@ print(dicionario_lambda["PRECO"]) # Quando a gente usa uma chave que não existe
 
 print(dicionario_lambda)
 # %%
+
+# OrderedDict
+
+"""A sub-classe OrderecDict se recorda das posições de indice Chave dos dicionários
+
+A solução serve para que os elementos respeitem sempre a mesma ordem da qual foram inseridos
+
+"""
+
+from collections import OrderedDict
+
+dicionario_ordenado = OrderedDict()
+
+print(dicionario_ordenado)
+
+# Adiciona chaves e valor
+
+dicionario_ordenado["NOME"] = "SamartPhone Galax"
+dicionario_ordenado["MARCA"] = "Samsung"
+dicionario_ordenado["MODELO"] = "S25 ultra"
+
+# Verificar a ordem
+for chave, valor in dicionario_ordenado.items():
+    print(f"Chave:{chave}  Valor:{valor}")
+
+# Alterando um item
+
+print("\nApós alterar um item\n")
+
+dicionario_ordenado["MARCA"] = "Apple"
+
+for chave, valor in dicionario_ordenado.items():
+    print(f"Chave: {chave}   Valor: {valor}")
+
+"""O que aconteceu aqui foi que a implementação de um novo elemento não alterou a ordem
+ de como o dicionário é escrito"""
+
+# Remover um elemento
+
+print("\nApós remover uma chave \n")
+
+dicionario_ordenado.pop("MARCA")
+
+for chave, valor in dicionario_ordenado.items():
+    print(f"Chave: {chave}    Valor: {valor}")
+
+## Após eu remover a chave marca e adicionar de novo...A ordem vai se manter?
+
+print("\nApós reinserir\n")
+
+dicionario_ordenado["MARCA"] = "Samsung"
+
+
+for chave, valor in dicionario_ordenado.items():
+    print(f"Chave: {chave}    Valor: {valor}")
+
+    # A ordem foi alterada....
+
+"""Usado apenas em um cenário na onde é necessario manter a ordem das chaves"""
+
+# %%
