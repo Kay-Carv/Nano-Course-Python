d193c375ec6fdc67b2695ebb049ddcb601c3d546
Kayque <kayquecarvalho020@gmail.com>
2025-05-14 23:56:56 -0300

    Feat: Docuentação de função e Script com função dentro de função

--- 04-Funcoes.py
+++ 04-Funcoes.py
@@ -96,6 +96,7 @@ exibe_ficha(**ficha_cliente)
 
 # Criando chave e valor em uma variável
 exibe_ficha(nome="Binca Jesus", estado_civil="solteiro(a)")
+exibe_ficha() # Retorna chave vazia
 
 # %%
 
@@ -114,3 +115,77 @@ resposta = soma(valor1, valor2)
 print(f"A variável resposta recebeu o return da função soma() e agora contém: {resposta}")
 print(f"A função soma() está sendo chamada dentro deste print para os valores 10 e 15 e retornou: {soma(10, 15)}")
 
+# %%
+
+# -------------------------------------------------------------------------# 
+# Um script de funções
+# -------------------------------------------------------------------------# 
+
+
+# Função calcula média de 
+def calcular_velocidade_media(distancia:float, tempo:float, unidade_medida="km/h"):
+    if tempo == 0:
+        return
+    velocidade_media = distancia / tempo
+    return f"{velocidade_media} {unidade_medida}"
+
+# Função Converte temperatura
+def converter_temperatura(temperatura:float, unidade_medida="celsius"):
+    if unidade_medida == "celsius":
+        return temperatura * 1.8 + 32
+    elif unidade_medida == "fahrenheit":
+        return (temperatura - 32) / 1.8
+    else:
+        return 0
+    
+def exibir_menu():
+    print("MENU")
+    print("1 - Calcular a velocidade média ")
+    print("2 - Converter a temperatura ")
+    print("3 - Sair")
+
+# Função dentro de função
+def aluno_de_fisica():
+    op = 0
+    while op != 3:
+        exibir_menu()
+        op = int(input("Informe a opção desejada: "))
+        if op == 1:
+            distancia_percorrida = float(input("Informe a distância: "))
+            tempo_viagem = float(input("Iforme o tempo da viagem: "))
+            # medida = input("Informe a unidade de medida: ")
+            print(f"\nA velocidade média é {calcular_velocidade_media(distancia_percorrida, tempo_viagem)}\n")
+        elif op == 2:
+            temperatura_informada = float(input("Informe a temperatura que deseja converter: "))
+            medida = input("A temperatura informada está em celsius ou fahrenheit")
+            print(f"\nO resultado da conversão é {converter_temperatura(temperatura_informada, medida)}\n")
+        elif op == 3:
+            print("\nSaindo do sistema...\n")
+            break
+        else:
+            print("Opção inválida!")
+
+aluno_de_fisica()
+# Também é possivel fazer essa requisição de um função através de um arquivo externo
+
+# import nome_do_arquivo
+
+#nome_do_arquivo.aluno_de_fisica()
+
+# Dessa forma você usa arquivos externos para rodar o código presente dentro desse arquivo
+# OBS: evite que o arquivo esteja renomeado com "-" e também evite começando com números   
+
+
+# %%
+
+# -------------------------------------------------------------------------# 
+# Documentar funções
+# -------------------------------------------------------------------------# 
+
+def somar(a:float, b:float):
+    """Recebe dois argumentos, a e b, tipo float e realiza a soma.
+     Retorna o resultado no formato float"""  # Exemplo de documentação do que a função faz
+    return a + b
+
+print(f"Chamando o método doc para ler a documentação: {somar.__doc__}") # Para consutar essa documentação é possivel usar o método __doc__
+print(f"Chamando a função help para ler a documentação: {help(somar)}")  # Também é possível usar help(função)
